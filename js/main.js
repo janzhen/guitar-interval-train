16baa6f5e74f6f01e54c0b00a83ebf644490eb7a
Zhen Zhijian <jan365@gmail.com>
2020-08-18 18:19:42 +0800

    Bugfix in board coordination, and draw all notes

--- js/main.js
+++ js/main.js
@@ -31,8 +31,8 @@ class Board {
             this.drawLine(i * this.cell_width, 0, i * this.cell_width, this.canvas.height)
         }
         // draw root note
-        this.highlight(4, 4, this.root_color)
-        this.drawNote(4, 4, '1')
+        this.highlight(3, 3, this.root_color)
+        this.drawNote(3, 3, '1')
     }
     drawLine(x1, y1, x2, y2) {
         const ctx = this.canvas.getContext("2d")
@@ -43,8 +43,8 @@ class Board {
         ctx.stroke()
     }
     highlight(row, col, color) {
-        const x = col * this.cell_width
-        const y = row * this.cell_height
+        const x = (col + this.margin) * this.cell_width
+        const y = (row + this.margin) * this.cell_height
         this.drawRect(x, y, this.cell_width, this.cell_height, color)
     }
     drawNote(row, col, note) {
@@ -54,8 +54,8 @@ class Board {
         ctx.textBaseline = 'top'
         ctx.fillStyle = this.front_color
         const width = ctx.measureText(note).width
-        const x = col * this.cell_width + (this.cell_width - width) / 2
-        const y = row * this.cell_height + height / 2
+        const x = (col + this.margin) * this.cell_width + (this.cell_width - width) / 2
+        const y = (row + this.margin) * this.cell_height + height / 2
         ctx.fillText(note, x, y)
     }
     drawRect(x, y, w, h, c) {
@@ -65,4 +65,35 @@ class Board {
     }
 }
 
+
+class IntervalGame {
+    constructor(board) {
+        this.board = board
+        this.notes = ['1', 'b3', '4', '5', 'b7']
+        this.position = [
+            [[0, 0], [1, 5], [5, 1], [6, 6]],  // 1
+            [[2, 1], [3, 6], [5, 4]],  // b3
+            [[2, 3], [4, 1], [5, 6]],  // 4
+            [[1, 0], [2, 5], [4, 3]],  // 5
+            [[1, 3], [3, 1], [4, 6]]  // b7
+        ]
+    }
+    drawAll() {
+        this.board.clear()
+        for (var i in this.notes) {
+            var n = this.notes[i]
+            for (var j in this.position[i]) {
+                var pos = this.position[i][j]
+                if (n === '1') {
+                    this.board.highlight(pos[0], pos[1], this.board.root_color)
+                }
+                this.board.drawNote(pos[0], pos[1], n)
+            }
+        }
+    }
+}
+
+
 const board = new Board('.container')
+const game = new IntervalGame(board)
+game.drawAll()
